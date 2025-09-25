f7c3f02441c2f481503c71e184a19c1faa7ec657
Alain Hebert <alainhebert@Alains-MacBook-Pro.local>
2025-09-25 08:53:20 -0400

    Forgot to commit CMakeLists.txt

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/alainhebert/Njoy_2016/scratch_branch/NJOY2016
-# Build directory: /Users/alainhebert/Njoy_2016/scratch_branch/NJOY2016/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
